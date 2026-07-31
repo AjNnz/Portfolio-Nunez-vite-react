e40b1608949d49be470db23195591576bf862d13
AjNnz <angelitonunez87@gmail.com>
2026-07-31 21:12:40 +0800

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
 export default {
   content: [
     "./index.html",
@@ -21,4 +22,4 @@ export default {
       'gray-600': '#334155',
     },
   },
-}
+} satisfies Config
